bbfbf8e4d18f9aea3bf28d790c9b6c7bc9dd5480
TILMATINE djawed eddine <159119324+TilmarineDjawed@users.noreply.github.com>
2024-05-20 12:58:00 +0100

    pfe

---
+++ app/src/main/java/com/example/suivieparenatl/certificat.java
@@ -0,0 +1,29 @@
+package com.example.suivieparenatl;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+
+public class certificat extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_certificat, container, false);
+
+
+
+        WebView webView = findViewById(R.id.web); // Remplacez "R.id.web" par l'ID de votre WebView
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://docs.google.com/gview?embedded=true&url=https://pdfurl.com/file.pdf");
+    }
+}
